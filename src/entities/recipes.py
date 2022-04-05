ecda4f07bb8d2701beb9d5fd1f9499ca99306b82
weverhall <weverhall@gmail.com>
2022-04-05 10:15:47 +0300

    wk3 dir removal

--- src/entities/recipes.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-class Recipes:
-
-    def __init__ (self, ingredients, howto, id=None):
-        self.ingredients = ingredients
-        self.howto = howto
-        self.id = id or str(uuid.uuid4())

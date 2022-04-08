a6ad63b9eb6883262b3b0fe1f3f1fa0518d0ae44
weverhall <weverhall@gmail.com>
2022-04-08 21:06:41 +0300

    wk4 new project

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -1,9 +1,8 @@
 from database_connection import get_database_connection
 
 def create_tables(connection):
-    cur = connection.cursor()
+    cursor = connection.cursor()
 
-<<<<<<< HEAD
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS domains (
             id INTEGER PRIMARY KEY,
@@ -18,31 +17,12 @@ def create_tables(connection):
             domain_id INTEGER REFERENCES domains
         );
     ''')
-=======
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS Recipes (
-            id INTEGER PRIMARY KEY NOT NULL,
-            title TEXT,
-            instructions TEXT
-        );
-    ''')
-
-    #cur.execute('''
-    #    CREATE TABLE IF NOT EXISTS Ingredients (
-    #        name TEXT PRIMARY KEY,
-    #        recipe_id INTEGER REFERENCES Recipes(id)
-    #    );
-    #''')
->>>>>>> 4aaace358a7d0c2ff4fec776779a4620c9ff7304
-
 
     connection.commit()
-
 
 def initialize_database():
     connection = get_database_connection()
     create_tables(connection)
-
 
 if __name__ == "__main__":
     initialize_database()
